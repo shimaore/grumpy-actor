b0a92fcb338907bf1cc25fc42a0d2942d845cc3d
Stephane Alnet <stephane@shimaore.net>
2019-04-04 11:59:08 +0200

    as-built

--- server.js
+++ server.js
@@ -72,7 +72,7 @@
         },
         summarize: {
           reduce: '_stats',
-          map: '(doc) ->\n  emit [doc.A,doc.S], doc.a'
+          map: '(doc) ->\n  emit [doc.c,doc.A,doc.S], doc.a'
         }
       },
       validate_doc_update: '-> require(\'views/lib/cdr\').validate_user_doc.apply this, arguments'
